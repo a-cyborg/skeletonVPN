62124e9215b8143587c5b805129c22ed7899c511
Oooh_chew <gx_339-4@pm.me>
2022-12-02 20:05:24 +0900

    Util: refactor NotificationUtils

--- app/src/main/java/org/cyb/skeletonvpn/util/NotificationUtils.kt
+++ app/src/main/java/org/cyb/skeletonvpn/util/NotificationUtils.kt
@@ -8,30 +8,33 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import org.cyb.skeletonvpn.MainActivity
 
-val NOTIFICATION_ID = 333 // In this app we handle one and only notification.
+const val NOTIFICATION_ID = 333 // In this app, we handle one and only notification.
 
-private val CHANNEL_ID = "Skeleton_Vpn"
-private val REQUEST_CODE = 0
-private val CLIENT_ACTIVITY = MainActivity::class.java
+private const val CHANNEL_ID = "Skeleton_Vpn"
+private const val REQUEST_CODE = 0
 
 fun updateNotification(msg: Int, context: Context) {
     NotificationManagerCompat.from(context)
         .notify(NOTIFICATION_ID, getNotification(msg, context))
 }
 
-fun getNotification(msg: Int, context: Context) : Notification {
-    // Intent to be sent when the notification is clicked.
-    val contentIntent = PendingIntent.getActivity(
-        context,
-        REQUEST_CODE,
-        Intent(context, CLIENT_ACTIVITY),
-        PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
+fun getNotification(msg: Int, context: Context): Notification {
     return NotificationCompat.Builder(context, CHANNEL_ID)
         .setShowWhen(false)
         .setSmallIcon(androidx.appcompat.R.drawable.abc_star_half_black_48dp)
-        .setContentIntent(contentIntent)
+        .setContentIntent(getContentIntent(context))
         .setContentText(context.getString(msg))
         .build()
+}
+
+// Intent to be sent when the notification is clicked.
+private fun getContentIntent(context: Context): PendingIntent {
+    val clientActivity = MainActivity::class.java
+
+    return PendingIntent.getActivity(
+        context,
+        REQUEST_CODE,
+        Intent(context, clientActivity),
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 }
